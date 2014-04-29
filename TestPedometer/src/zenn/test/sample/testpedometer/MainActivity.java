ae749e19706d80894ef9a772ae875240c9d1b87d
settnr <settnr@gmail.com>
2014-04-29 17:45:39 +0900

    センターのリスト、およびX,Y,Z軸の加速度を表示するようにした。

--- TestPedometer/src/zenn/test/sample/testpedometer/MainActivity.java
+++ TestPedometer/src/zenn/test/sample/testpedometer/MainActivity.java
@@ -1,27 +1,51 @@
 package zenn.test.sample.testpedometer;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+
+	/** Called when the activity is first created. */
+	SensorAdapter ad;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d("MainActivity", "onCreate");
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ALL);
+		TextView comment = (TextView) findViewById(R.id.comment);
+		String val = "サポートセンサー\n";
+		for (Sensor s : sensors) {
+			val += s.getName() + "\n";
+		}
+		comment.setText(val);
+
+		ad = new SensorAdapter(findViewById(R.id.lvaluex),
+				findViewById(R.id.lvaluey), findViewById(R.id.lvaluez));
+
 	}
 
 	@Override
@@ -61,4 +85,45 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	class SensorAdapter implements SensorEventListener{
+
+		private SensorManager manager;
+		private TextView vx;
+		private TextView vy;
+		private TextView vz;
+
+		public SensorAdapter(View vx, View vy, View vz) {
+			manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+			this.vx = (TextView) vx;
+			this.vy = (TextView) vy;
+			this.vz = (TextView) vz;
+			List<Sensor> sensors = manager
+					.getSensorList(Sensor.TYPE_ACCELEROMETER);
+			if (sensors.size() > 0) {
+				Sensor s = sensors.get(0);
+				manager.registerListener(this, s, SensorManager.SENSOR_DELAY_UI);
+			}
+		}
+
+		public void stopSensor() {
+			manager.unregisterListener(this);
+			this.vx = null;
+			this.vy = null;
+			this.vz = null;
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor arg0, int arg1) {
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+				vx.setText("" + event.values[0]);
+				vy.setText("" + event.values[1]);
+				vz.setText("" + event.values[2]);
+			}
+		}
+	}
 }
